8b5e8faeff6b0fd1c778b18dddb9cdf6f3c47313
sorim lee <lsr_1105@ewhain.net>
2022-01-09 00:31:38 +0900

    fix: use bisec_left library
    -정확성: 40
    -효율성: 60
    - 총점:100 통과

--- python/search_rating.py
+++ python/search_rating.py
@@ -1,5 +1,6 @@
 from itertools import combinations
 from collections import Counter
+from bisect import bisect_left
 
 def split_st(q):
     st = q.replace('and','').split()
@@ -29,20 +30,15 @@ def solution(info, query):
                     in_data[tmp]=[score]
     
     for v in in_data.values():
-        v.sort()        
+        v.sort()
         
     for q in query:
         st, score = split_st(q)
-        if st in list(in_data):
+        if st in in_data:
             data = in_data[st]
-            if len(data)>0:
-                start,end=0, len(data)
-                while start!=end and start!=len(data):
-                    if int(data[(start+end)//2]) >= score:
-                        end = (start+end) //2
-                    else:
-                        start = (start+end)//2+1
-                answer.append(len(data)-start)
-            else:
-                answer.append(0)
+            index = bisect_left(data, score)
+            answer.append(len(data) - index)
+        else:
+            answer.append(0)
+            continue
     return answer
