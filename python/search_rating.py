ce99d7d22f546eb287f90ab583b1bd56d3d3b197
sorim lee <lsr_1105@ewhain.net>
2022-01-08 23:58:51 +0900

    fix: use combination

--- python/search_rating.py
+++ python/search_rating.py
@@ -5,21 +5,40 @@ def split_st(q):
     st = q.split('and')
     sv = str(''.join(st))
     st = sv.split()
-    return st
+    score = int(st[-1])
+    while '-' in st:
+        st.remove('-')
+    st.remove(st[-1])
+    st.sort()
+    st=str(''.join(st))
+    return st,score
 
 def solution(info, query):
     answer = []
-    for q in query:
-        st = split_st(q)
-        num=0
-        for i in info:
-            check = True
-            for k in range(len(st)-1):
-                if st[k]!='-' and st[k] not in i:
-                    check= False
-            it = i.split()
-            if check == True and int(st[-1])<=int(it[-1]):
-                num+=1
-        answer.append(num)
+    in_data={}
         
+    for i in info:
+        num=0
+        it=i.split()
+        for j in range(5):
+            combi=list(combinations(sorted(it[:-1]),j))
+            for c in combi:
+                tmp = ''.join(c)
+                in_data[tmp]=it[-1]
+    
+    for q in query:
+        st, score = split_st(q)
+        if st in list(in_data):
+            data = in_data[st]
+        
+            if len(data)>0:
+                start,end=0, len(data)
+                while start!=end and start!=len(data):
+                    if int(data[(start+end)//2]) >= score:
+                        end = (start+end) //2
+                    else:
+                        start = (start+end)//2+1
+                answer.append(len(data)-start)
+            else:
+                answer.append(0)
     return answer
