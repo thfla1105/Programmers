d773bb2857e225aabf4958991dc587d43cc373bd
sorim lee <lsr_1105@ewhain.net>
2022-01-13 19:03:08 +0900

    fix: use heapq module

--- python/carpool_taxi_price.py
+++ python/carpool_taxi_price.py
@@ -1,4 +1,4 @@
-from collections import deque
+import heapq
 
 INF = int(1e9)
 
@@ -30,28 +30,21 @@ def solution(n, s, a, b, fares):
 
 def dijkstra(n,graph,s):
     distance=[INF]*(n+1)
-    visited=[False]*(n+1)
     
     distance[s]=0
-    visited[s]=True
     
-    for k,v in graph[s].items():
-        distance[k]=v
-        
-    for i in range(n-1):
-        min_v=INF
-        tmp=0
-        for l in range(1,n+1):
-            if distance[l]<min_v and not visited[l]:
-                min_v=distance[l]
-                tmp=l
-
-        visited[tmp]=True
-        if tmp!=0:
-            for key,value in graph[tmp].items():
-                cost = distance[tmp] + value
-                if cost < distance[key]:
-                    distance[key]=cost
+    queue=[]
+    heapq.heappush(queue,[distance[s],s])
     
+    while queue:
+        tmp_dist,tmp_dest=heapq.heappop(queue)
+        if distance[tmp_dest]<tmp_dist:
+            continue
+        for next_dest,next_dist in graph[tmp_dest].items():
+            dist = tmp_dist+next_dist
+            if dist < distance[next_dest]:
+                distance[next_dest]=dist
+                heapq.heappush(queue,[dist,next_dest])
+                
     return distance
             
