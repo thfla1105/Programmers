94f68f494916ebc6c924d85cb45d481aa8ac1593
sorim lee <lsr_1105@ewhain.net>
2022-01-18 00:29:32 +0900

    fix: fix algorithm

--- python/insert_ad.py
+++ python/insert_ad.py
@@ -6,8 +6,6 @@ def solution(play_time, adv_time, logs):
     logs_sec=[[str_to_int(y) for y in x.split('-')] for x in logs]
     all_sec=[0]*(play_sec+1)
     
-    sorted(logs_sec)
-    
     for log in logs_sec:
         all_sec[log[0]]=all_sec[log[0]]+1
         all_sec[log[1]]=all_sec[log[1]]-1
@@ -18,8 +16,19 @@ def solution(play_time, adv_time, logs):
     for p in range(1,play_sec+1):
         all_sec[p]=all_sec[p]+all_sec[p-1]
     
+    most_view=0
+    max_sec=0
     
-    answer=int_to_str(idx)
+    for i in range(adv_sec-1,play_sec):
+        if i >=adv_sec:
+            if most_view <all_sec[i]-all_sec[i-adv_sec]:
+                most_view=all_sec[i]-all_sec[i-adv_sec]
+                max_sec=i-adv_sec+1
+        else:
+            if most_view<all_sec[i]:
+                most_view=all_sec[i]
+                max_sec=i-adv_sec+1
+    answer=int_to_str(max_sec)
     return answer
 
 
@@ -31,6 +40,6 @@ def str_to_int(time):
 def int_to_str(second):
     mm, ss = divmod(second, 60)
     hh, mm = divmod(mm, 60)
-    time='{}:{:0>2}:{:0>2}'.format(hh, mm, ss)
+    time='{:0>2}:{:0>2}:{:0>2}'.format(hh, mm, ss)
     return time
     
