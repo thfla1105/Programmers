e857d7706306a8bd6eeef3eb3ce9dc35f299c6b1
SR <lsr_1105@ewhain.net>
2022-01-30 17:41:48 +0900

    fix: fix algorithm for searh_lyrics.py
    - 정확성: 30.0 /30.0
    - 효율성: 25.0/70.0 (효율성 4,5번만 통과)

--- python/search_lyrics.py
+++ python/search_lyrics.py
@@ -1,22 +1,21 @@
 def solution(words, queries):
     answer = []
+    word={w for w in words}
+    word=sorted(word)
     for q in queries:
         st=q.split("?")
         num=0
         if st[0]=='':
-            st=list(set(st))
-            st.remove('')
-            for w in words:
-                if w.endswith(st[0]) and len(w)==len(q):
-                    
+            s=st[-1]
+            for w in word:
+                if w.endswith(s) and len(w)==len(q):
                     num+=1
-        
+                    
             answer.append(num)
         else:
-            st=list(set(st))
-            st.remove('')
-            for w in words:
-                if w.startswith(st[0]) and len(w)==len(q):
+            s=st[0]
+            for w in word:
+                if w.startswith(s) and len(w)==len(q):
                     num+=1
                     
             answer.append(num)
