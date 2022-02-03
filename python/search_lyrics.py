f70936cceed9e7224883489fb6c494a2f1afa325
SR <lsr_1105@ewhain.net>
2022-02-03 23:17:32 +0900

    feat: add bisect for search_lyrics.py
    - 정확도 25.0/25.0
    - 효율성 4,5 통과

--- python/search_lyrics.py
+++ python/search_lyrics.py
@@ -1,24 +1,30 @@
+from collections import defaultdict
+from bisect import bisect_left, bisect_right
+
+def count_by_range(a,left_val,right_val):
+    left_idx=bisect_left(a,left_val)
+    right_idx=bisect_right(a,right_val)
+    return right_idx-left_idx
+
 def solution(words, queries):
     answer = []
-    word={w for w in words}
-    word=sorted(word)
+    word=defaultdict(list)
+    reverse_word=defaultdict(list)
+    for w in words:
+        word[len(w)].append(w)
+        reverse_word[len(w)].append(w[::-1])
+    for w in words:
+        word[len(w)].sort()
+        reverse_word[len(w)].sort()
     for q in queries:
-        st=q.split("?")
         num=0
-        if st[0]=='':
-            s=st[-1]
-            for w in word:
-                if w.endswith(s) and len(w)==len(q):
-                    num+=1
-                    
-            answer.append(num)
+        if q[0]=='?':
+            num=count_by_range(reverse_word[len(q)],q[::-1].replace('?','a'),q[::-1].replace('?','z'))
         else:
-            s=st[0]
-            for w in word:
-                if w.startswith(s) and len(w)==len(q):
-                    num+=1
+            num=count_by_range(word[len(q)],q.replace('?','a'),q.replace('?','z'))
                     
-            answer.append(num)
+        answer.append(num)
                     
         
     return answer
+
