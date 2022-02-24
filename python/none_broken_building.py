62be5d0f5f552e8a6d39990012f72d31779c0de5
SR <lsr_1105@ewhain.net>
2022-02-24 23:55:08 +0900

    fix: change algorithm using accumulate
    -정확성, 효율성 100

--- python/none_broken_building.py
+++ python/none_broken_building.py
@@ -1,25 +1,29 @@
-from collections import deque
-from itertools import product
-
 def solution(board, skill):
     answer = 0
     row=len(board)
     col=len(board[0])
-    broken= set()
-    for s in skill:
-        tp,r1,c1,r2,c2,degree=s[0],s[1],s[2],s[3],s[4],s[5]
-        rc=[[i for i in range(r1,r2+1)],[j for j in range(c1,c2+1)]]
-        q=set(product(*rc))
+    building=[[0 for _ in range(col+1)] for _ in range(row+1)]
+    for tp,r1,c1,r2,c2,degree in skill:
         if tp==1:
             degree=-1*degree
-        while q:
-            tmp=q.pop()
-            board[tmp[0]][tmp[1]]+=degree
-            if tmp not in broken and board[tmp[0]][tmp[1]]<=0:
-                broken.add(tmp)
-            if tmp in broken and board[tmp[0]][tmp[1]]>0:
-                broken.remove(tmp)
+        building[r1][c1]+=degree
+        building[r1][c2+1]-=degree
+        building[r2+1][c1]-=degree
+        building[r2+1][c2+1]+=degree
     
-    answer=row*col - len(broken)
+    for j in range(col):
+        for i in range(row):
+            building[i+1][j]+=building[i][j]
+    
+    for i in range(row):
+        for j in range(col):
+            building[i][j+1]+=building[i][j]
+            
+    for i in range(row):
+        for j in range(col):
+            board[i][j]+=building[i][j]
+            if board[i][j]>0:
+                answer+=1
+        
         
     return answer
