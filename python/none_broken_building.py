db0637d8a294729fccc3159d9686c48f3b0aa52e
SR <lsr_1105@ewhain.net>
2022-02-24 23:08:21 +0900

    fix: change product to set for time complexity

--- python/none_broken_building.py
+++ python/none_broken_building.py
@@ -5,8 +5,7 @@ def solution(board, skill):
     answer = 0
     row=len(board)
     col=len(board[0])
-    rc_board=[[i for i in range(row)],[j for j in range(col)]]
-    result= set(product(*rc_board))
+    broken= set()
     for s in skill:
         tp,r1,c1,r2,c2,degree=s[0],s[1],s[2],s[3],s[4],s[5]
         rc=[[i for i in range(r1,r2+1)],[j for j in range(c1,c2+1)]]
@@ -16,11 +15,11 @@ def solution(board, skill):
         while q:
             tmp=q.pop()
             board[tmp[0]][tmp[1]]+=degree
-            if tmp not in result and board[tmp[0]][tmp[1]]>0:
-                result.add(tmp)
-            if tmp in result and board[tmp[0]][tmp[1]]<=0:
-                result.remove(tmp)
+            if tmp not in broken and board[tmp[0]][tmp[1]]<=0:
+                broken.add(tmp)
+            if tmp in broken and board[tmp[0]][tmp[1]]>0:
+                broken.remove(tmp)
     
-    answer=len(result)
+    answer=row*col - len(broken)
         
     return answer
